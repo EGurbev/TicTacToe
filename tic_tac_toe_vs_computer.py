b2fc10bb28bcc6946b70c2759db2511caedd85b6
EGurbev <117995366+EGurbev@users.noreply.github.com>
2023-01-29 14:44:29 +0200

    Add files via upload

---
+++ tic_tac_toe_vs_computer.py
@@ -0,0 +1,132 @@
+import random
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+player = "X"
+computer = "O"
+current_player = player
+winner = None
+game_running = True
+
+# print board
+
+
+def print_board(field):
+    print(f"{field[0]} | {field[1]} | {field[2]} |")
+    print(f"{field[3]} | {field[4]} | {field[5]} |")
+    print(f"{field[6]} | {field[7]} | {field[8]} |")
+
+
+# player input
+
+
+def player_move(field):
+    valid_move = False
+    while not valid_move:
+        move = int(input("Enter a number 1-9: "))
+        if 0 <= move <= 9:
+            if field[move-1] == "-":
+                field[move-1] = player
+                valid_move = True
+            else:
+                print("Invalid move!")
+        else:
+            print("Invalid move!")
+            continue
+
+
+# Computer move
+
+
+def computer_move(field):
+    print("Computer move:")
+    valid_move = False
+    while not valid_move:
+        move = random.randint(1, 9)
+        if field[move-1] == "-":
+            field[move-1] = computer
+            valid_move = True
+        else:
+            continue
+
+
+def switch_player():
+    global current_player
+    if current_player == player:
+        player_move(board)
+        current_player = computer
+    else:
+        computer_move(board)
+        current_player = player
+
+
+# check for win or tie
+
+
+def check_horizontal(field):
+    global winner
+    if field[0] == field[1] == field[2] and field[0] != "-":
+        winner = field[0]
+        return True
+    elif field[3] == field[4] == field[5] and field[3] != "-":
+        winner = field[3]
+        return True
+    elif field[6] == field[7] == field[8] and field[6] != "-":
+        winner = field[6]
+        return True
+
+
+def check_vertical(field):
+    global winner
+    if field[0] == field[3] == field[6] and field[0] != "-":
+        winner = field[0]
+        return True
+    elif field[1] == field[4] == field[7] and field[1] != "-":
+        winner = field[1]
+        return True
+    elif field[2] == field[5] == field[8] and field[2] != "-":
+        winner = field[2]
+        return True
+
+
+def check_diagonal(field):
+    global winner
+    if field[0] == field[4] == field[8] and field[0] != "-":
+        winner = field[0]
+        return True
+    elif field[2] == field[4] == field[6] and field[2] != "-":
+        winner = field[2]
+        return True
+
+
+def check_tie():
+    global winner
+    if "-" not in board:
+        winner = None
+        return True
+
+
+def check_winner():
+    global game_running
+    if check_horizontal(board) or check_vertical(board) or check_diagonal(board):
+        game_running = False
+        if winner == "X":
+            print(f"\nWinner: Player ({player})")
+            print_board(board)
+        else:
+            print(f"\nWinner: Computer ({computer})")
+            print_board(board)
+    elif check_tie():
+        game_running = False
+        print("Draw!")
+        print_board(board)
+
+
+# The game
+
+while game_running:
+    print_board(board)
+    switch_player()
+    check_winner()
